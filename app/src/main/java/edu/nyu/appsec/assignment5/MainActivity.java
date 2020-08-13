ac26711e01ce207c5b1976928dad9afa0d66dd3a
Fariha Syed <fs1688@nyu.edu>
2020-08-13 16:26:46 -0400

    Removed vulnerabilities, improper operations, and unnecessary code

--- app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
+++ app/src/main/java/edu/nyu/appsec/assignment5/MainActivity.java
@@ -12,22 +12,15 @@ import android.net.http.SslError;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.SerializablePermission;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-    private static final String SPELL_CHECK_URL = "http://appsecclass.report:8080/";
-    private static final String KNOWN_HOST = "appsecclass.report";
+public class MainActivity extends AppCompatActivity {
+    private static final String SPELL_CHECK_URL = "http://10.0.2.2:5000/";
+    private static final String KNOWN_HOST = "10.0.2.2";
 
     private class MyWebViewClient extends WebViewClient {
         @Override
@@ -45,42 +38,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
     }
 
-    /* Get location data to provide language localization
-    *  Supported languages ar-DZ zh-CN en-US en-IN en-AU fr-FR
-    */
-    @Override
-    public void onLocationChanged(Location location) {
-        URL url = null;
-        try {
-            url = new URL(SPELL_CHECK_URL + "metrics"
-                    +"?lat="
-                    +location.getLatitude()+"&long=" + location.getLongitude()
-            );
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        HttpURLConnection urlConnection = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /* Necessary to implement the LocationListener interface
-    */
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {}
-
-    @Override
-    public void onProviderEnabled(String s) {}
-
-    @Override
-    public void onProviderDisabled(String s) {}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,15 +47,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         view.setWebViewClient(new MyWebViewClient());
 
         WebSettings settings = view.getSettings();
-        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowFileAccessFromFileURLs(false);
         settings.setJavaScriptEnabled(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
-
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        }
+        settings.setAllowUniversalAccessFromFileURLs(false);
 
         setContentView(view);
         view.loadUrl(SPELL_CHECK_URL + "register");
